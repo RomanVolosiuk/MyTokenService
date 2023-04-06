0751fc444abbb30007cc4df01ffcd95327d800ac
VolosiukRoman <rbulany@gmail.con>
2023-04-06 15:46:21 +0300

    Improved TokenService added: password verifier, user verifier, string to hash MD5 method Improved ControllerAdvisor: added user verifier handlers exception

---
+++ src/main/java/ua/volosiuk/mytokenservice/exception/NoMD5AlgorithmException.java
@@ -0,0 +1,8 @@
+package ua.volosiuk.mytokenservice.exception;
+
+import java.security.NoSuchAlgorithmException;
+
+public class NoMD5AlgorithmException extends RuntimeException {
+
+    public NoMD5AlgorithmException() {}
+}
